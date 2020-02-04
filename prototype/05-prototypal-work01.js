be8cfb5f35a9bf3665eb2741007f419d29a809c0
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2020-02-04 10:59:25 -0400

    add prototypal inheritance examples 2

---
+++ prototype/05-prototypal-work01.js
@@ -0,0 +1,13 @@
+//Exercise - extend the functionality of a built in object
+
+//#1
+//Date object => to have new method .lastYear() which shows you last year 'YYYY' format.
+
+new Date('1900-10-10').lastYear()
+//'1899'
+
+
+//#Bonus
+// Mofify .map() to print '🗺' at the end of each item.
+console.log([1,2,3].map())
+//1🗺, 2🗺, 3🗺
